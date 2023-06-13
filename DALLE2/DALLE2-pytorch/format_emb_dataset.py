fd5e551620b8808362cd765f28a8641eb56d63b7
chenbo2 <chenbo2@tal.com>
2023-06-14 00:52:47 +0800

    并行推理embedding

--- DALLE2/DALLE2-pytorch/format_emb_dataset.py
+++ DALLE2/DALLE2-pytorch/format_emb_dataset.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 import numpy as np
 import torch
 import os
-from multiprocessing import Process, Manager
+import sys
 
 
 meta_data_path = "../data/imgs/"
@@ -18,7 +18,7 @@ def split2sublist(arr, split_num):
         ret[idx % split_num].append(val)
     return ret
 
-def inference(files, device, info_dict):
+def inference(files, device):
     clip = HuggingfaceAdapter(
             text_encoder_name = '/data/Data/ye/zyj/PTMs/Taiyi-CLIP-Roberta-large-326M-Chinese',
             img_encoder_name = '/data/Data/ye/zyj/PTMs/clip-vit-large-patch14'
@@ -39,7 +39,7 @@ def inference(files, device, info_dict):
         save_embs = [[], [], []]
         save_keys = []
         save_dirs = ["text_embs", "text_encodings", "img_embs"]
-        info_dict[filename] = 0
+        cnt = 0
         for keys, texts, imgs in tqdm(dataloader, desc=filename):
             texts = texts.to(device)
             imgs = imgs.to(device)
@@ -50,7 +50,7 @@ def inference(files, device, info_dict):
             save_embs[1].append(text_encodings)
             save_embs[2].append(img_embs)
             save_keys.extend(keys)
-            info_dict[filename] += len(keys)
+            cnt += len(keys)
 
         for dirname, embs in zip(save_dirs, save_embs):
             embs = torch.cat(embs).cpu().numpy()
@@ -59,40 +59,37 @@ def inference(files, device, info_dict):
                 save_keys[i]:embs[i] for i in range(len(save_keys))
             }
             np.save(os.path.join(output_dir, dirname, filename.replace(".tar", ".npy")), dic)
-
-
-def multi_processing(arr, thread_num, func):
-    sub_arrs = split2sublist(arr, thread_num)
-    manager = Manager()
-    return_dict = manager.dict()
-    jobs = []
-    for i in range(thread_num):
-        p = Process(target=func, args=(sub_arrs[i], f"cuda:{i}", return_dict))
-        jobs.append(p)
-        p.start()
-    for proc in jobs:
-        proc.join()
-    return return_dict
-
+        print(f"[Finished] {filename} cnt = {cnt}")
 
 if __name__ == "__main__":
-    # 创建output_dir
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
-        for dirname in ["text_embs", "text_encodings", "img_embs"]:
-            _path = os.path.join(output_dir, dirname)
-            if not os.path.exists(_path):
-                os.mkdir(_path)
-    # input files
-    filenames = []
-    for filename in os.listdir(meta_data_path):
-        if filename.endswith(".tar"):
-            filenames.append(filename)
-    print(f"files num = {len(filenames)}")
+    if len(sys.argv) == 1: # 主进程
+        # 创建output_dir
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+            for dirname in ["text_embs", "text_encodings", "img_embs"]:
+                _path = os.path.join(output_dir, dirname)
+                if not os.path.exists(_path):
+                    os.mkdir(_path)
+        # input files
+        filenames = []
+        for filename in os.listdir(meta_data_path):
+            if filename.endswith(".tar"):
+                filenames.append(filename)
+        print(f"files num = {len(filenames)}")
 
-    device_cnt = torch.cuda.device_count()    
-    print(f"Available devices = {device_cnt}")
+        device_cnt = torch.cuda.device_count()    
+        print(f"Available devices = {device_cnt}")
+
+        device_cnt = min(device_cnt, len(filenames))
+        sub_arrs = split2sublist(filenames, device_cnt)
+        for i in range(device_cnt):
+            os.system(f"python format_emb_dataset.py {','.join(sub_arrs[i])} cuda:{i} > logs/cuda:{i}.log 2>&1 &")
     
-    return_dict = multi_processing(filenames, device_cnt, inference)
-    for k in return_dict.keys():
-        print(k, return_dict[k])
+    elif len(sys.argv) == 3: # 推理进程
+        files = sys.argv[1].split(",")
+        device = sys.argv[2]
+        print(f"[Begin] {device} {files}")
+        inference(files, device)
+
+    else:
+        print("params error!")
